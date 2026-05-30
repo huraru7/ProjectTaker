44c27b495e53d943c7dd7100158b2f037dcffcad
huraru <hurarunium@gmail.com>
2026-05-30 22:55:20 +0900

    [Add]

--- Assets/ProjectTanker/Script/GameUI/Slot/SlotItemUI.cs
+++ Assets/ProjectTanker/Script/GameUI/Slot/SlotItemUI.cs
@@ -3,12 +3,15 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using ProjectTanker.UI;
 
 public class SlotItemUI : MonoBehaviour, IDropHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    //:装備スロット1つ分のUI
     [SerializeField] private Image iconImage;
     [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private Image accentLine;          // スロット下部の属性カラーライン
+    [SerializeField] private TextMeshProUGUI slotNumber; // スロット番号 (1–7)
+    [SerializeField] private ThemeColor theme;
 
     public ModuleData Data { get; private set; }
     public static int DraggingSlotIndex { get; private set; } = -1;
@@ -23,9 +26,20 @@ public class SlotItemUI : MonoBehaviour, IDropHandler, IBeginDragHandler, IDragH
         Data = data;
         _slotIndex = slotIndex;
         _onDrop = onDrop;
+
+        if (slotNumber != null)
+            slotNumber.text = (slotIndex + 1).ToString();
+
         bool isEmpty = data == null;
         iconImage.sprite = isEmpty ? null : data.icon;
         nameText.text = isEmpty ? "" : data.moduleName;
+
+        if (accentLine != null && theme != null)
+            accentLine.color = isEmpty ? theme.border : GetElementColor(data.moduleElement);
+
+        // 空スロットはアイコン半透明
+        var c = iconImage.color;
+        iconImage.color = new Color(c.r, c.g, c.b, isEmpty ? 0f : 1f);
     }
 
     public void OnDrop(PointerEventData eventData)
@@ -45,6 +59,7 @@ public class SlotItemUI : MonoBehaviour, IDropHandler, IBeginDragHandler, IDragH
         var img = _ghost.AddComponent<Image>();
         img.sprite = iconImage.sprite;
         img.raycastTarget = false;
+        img.color = new Color(1f, 1f, 1f, 0.7f);
         _ghost.GetComponent<RectTransform>().sizeDelta = new Vector2(60, 60);
 
         UpdateGhostPosition(eventData);
@@ -67,4 +82,13 @@ public class SlotItemUI : MonoBehaviour, IDropHandler, IBeginDragHandler, IDragH
             out var localPos);
         _ghost.GetComponent<RectTransform>().localPosition = localPos;
     }
+
+    private Color GetElementColor(ModuleElementEnum element) => element switch
+    {
+        ModuleElementEnum.Earth => theme.earth,
+        ModuleElementEnum.Water => theme.water,
+        ModuleElementEnum.Fire  => theme.fire,
+        ModuleElementEnum.Wind  => theme.wind,
+        _                       => theme.none,
+    };
 }
